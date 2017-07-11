f870fd9ce36659288ffcfb5cb9bca5eafa5a30d1
theo5970 <theo5970@naver.com>
2017-07-12 08:53:12 +0900

    주석 추가

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,46 @@
-function findIt() {
-    var context = $("input[id=whatFind]").val();
-    var data = {"context": context};
-    $.ajax({
-        type: "post",
-        url: "/find",
-        dataType: "json",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        success: function(data){
-            
-        },
-        data: data
-    });
-}
-
+// 글 올리기
 function postArticle() {
+    // ajax로 업로드
     $.ajax({
         type: "post",
         url: "/post",
         success: function(data) {
+            // 만약 글 올리는 데 성공하면
             if (data.isSuccess) {
+                // 에러 경고를 숨긴다
                 hideDiv("#error_inpost");
+
+                // 성공 경고를 표시한다
                 showDiv("#success_inpost");
                 
+                // 1초 후 페이지를 새로고침한다
                 setTimeout(function(){
                     location.reload();
                 }, 1000);
             } else {
+
+                // 에러 경고를 표시한다
                 showDiv("#error_inpost");
+
+                // 에러 경고의 내용을 서버로부터 온 에러 메시지로 설정한다
                 $("#error_inpost p").html(data.messages.join("<br/>"));
             }
         },
+        
+        // 서버에 보낼 데이터
         data: {
-            title: $("input[id=titleText]").val(),
-            context: $("textarea#contextText").val()
+            title: $("input[id=titleText]").val(),      // 글 제목
+            context: $("textarea#contextText").val()    // 글 내용
         }
     });
 }
 
-
+// div 표시하기
 function showDiv(id) {
     $(id).css("display", "block");
 }
+
+// div 숨기기
 function hideDiv(id) {
     $(id).css("display", "none");
 }
--- public/js/article_app.js
+++ public/js/article_app.js
@@ -1,45 +1,66 @@
+// 댓글 달기
 function postComment(article_dbid) {
+
+    // 댓글 내용
     let context = $("input[id="+article_dbid+"_comment]").val();
-    console.log(context);
+
+    // ajax로 업로드
     $.ajax({
         type: "post",
         url: "/post_comment",
         dataType: "json",
         success: function(data) {
+            // 댓글 올리는 데 성공하면
             if (data.isSuccess) {
+
+                // 페이지 새로고침
                 location.reload();
             } else {
+
+                // 에러 모달 표시
                 $("#error_incomment_modal").modal("show");
             }
         },
+
+        // 서버로 보낼 데이터
         data: {
-            _id: article_dbid,
-            context: context
+            _id: article_dbid,  // 데이터베이스 고유 ID
+            context: context    // 댓글 내용
         }
     });
 }
 
+// 좋아요 누르기
 function doLike(article_dbid) {
+    // ajax로 처리
     $.ajax({
         type: "post",
         url: "/post_like",
         dataType: "json",
         success: function(data) {
+            // 만약 성공하면
             if (data.isSuccess) {
                 console.log(data.count);
                 let btn = $("#likebtn_" + article_dbid);
+
+                // 좋아요가 눌러졌으면 버튼에 active 클래스를,
+                // 취소됬으면 버튼에 active 클래스를 제거한다.
                 if (data.isLiked) {
                     btn.addClass("active");
                 } else {
                     btn.removeClass("active");
                 }
+
+                // 버튼 텍스트를 좋아요 개수로 설정한다.
                 btn.find("span:nth-child(2)").html(data.count.toString());
             } else {
-                alert("이미 좋아요를 누르셨습니다");
+                alert("오류가 발생했습니다. 다시 시도해주세요");
             }
         },
+
+        // 서버로 보낼 데이터
         data: {
-            _id: article_dbid
+            _id: article_dbid   // 데이터베이스 고유 ID
         }
     })
 }
